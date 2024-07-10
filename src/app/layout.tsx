f2ba9cd907e7469dce2c06c0bcc7d0438559bf08
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-07-11 00:27:31 +0530

    added geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { Providers } from "./provider";
 import { ViewTransitions } from "next-view-transitions";
+import { GeistSans } from "geist/font/sans";
 const bricolage = Bricolage_Grotesque({
   subsets: ["latin"],
   weight: ["500", "800"],
@@ -22,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <ViewTransitions>
-      <html lang="en" className={bricolage.className}>
+      <html lang="en" className={GeistSans.className}>
       <body className="lg:max-w-[712px] w-full p-4 mx-auto min-h-screen">
         <Providers>
           <NavBar />
