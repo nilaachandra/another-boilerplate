9f804032ea3d654208d81c1f936578f7a57853f4
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-07-11 22:38:19 +0530

    signup backend created and tested

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,12 +1,36 @@
 "use server"
 import * as z from 'zod'
 import { SignupSchema } from '../schemas'
-
+import bcrypt from 'bcrypt'
+import prisma from '../prisma/client'
+import { error } from 'console'
+import { getUserByEmail } from '../data/user'
 
 export const signup = async (values: z.infer<typeof SignupSchema>) => {
     const validatedFields = SignupSchema.safeParse(values);
     if (!validatedFields.success) {
         return { error: "Invalid Fields!" }
     }
+
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    //function to check existing user
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: "Email already in use!" }
+    }
+
+    //creating the user
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+
+        //TODO : Send Verification Email
+    })
     return { success: "Succesfully Created Account" }
 }
